06a4b6d2d319b891529b0a708f67e9a6b440ba9c
ybivator <htonasral@ukr.net>
2013-06-05 18:11:24 +0300

    Beginning of the project - 3.17

---
+++ 3.17/src/harm.cpp
@@ -0,0 +1,21 @@
+#include <GL/glut.h>
+
+
+int main(int argc, char *argv[])
+{
+   int windowWidth = 1000;
+   int windowHeight = 800;
+   int windowPosX = 0;
+   int windowPosY = 0;
+
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+   glutInitWindowSize(windowWidth, windowHeight);
+   glutInitWindowPosition(windowPosX, windowPosY);
+   glutCreateWindow("3.17");
+
+
+   glutMainLoop();
+
+   return 0;
+}
